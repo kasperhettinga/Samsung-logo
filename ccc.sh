342b033d0e523fa1c666021f90c005181e8560cc
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-04-30 13:54:45 +0200

    Update logo & script

--- ccc.sh
+++ ccc.sh
@@ -4,8 +4,8 @@
 #
 
 ./createLogos.sh
-cp logo_rgb24_wvga_portrait_custom.h ~/android/system/kernel/samsung/aries/drivers/video/samsung/logo_rgb24_wvga_portrait.h
-cd ~/android/system/kernel/samsung/aries/
+cp logo_rgb24_wvga_portrait_custom.h ~/android/cm101/kernel/samsung/aries/drivers/video/samsung/logo_rgb24_wvga_portrait.h
+cd ~/android/cm101/kernel/samsung/aries/
 git add drivers/video/samsung/logo_rgb24_wvga_portrait.h
 git commit -m "Update boot logo"
 cd ~/android/logo
